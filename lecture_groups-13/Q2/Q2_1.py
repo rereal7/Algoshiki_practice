e07893aff5ac527b1cd4a4767b4f487c5b52ba1f
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-10-31 04:38:09 +0900

    ?

---
+++ lecture_groups-13/Q2/Q2_1.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+left = 0
+right = 100
+while right - left > 1e-4:
+	mid = (right + left) / 2
+	f = ((mid+1)*mid +2)*mid + 3
+	if f < n:
+		left = mid
+	else:
+		right = mid
+
+ans = left 
+print(ans)
