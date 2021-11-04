9d9654ba88b1b525bd7e28ef9a96ffedf6313895
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-11-05 01:44:48 +0900

    add

---
+++ lecture_groups-13/Q2/Q2_4.py
@@ -0,0 +1,15 @@
+n,m = map(int, input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+A.sort()
+
+for i in range(m):
+	left = 0
+	right = n
+	while left != right:
+		mid = (left + right) // 2
+		if A[mid] > B[i]:
+			right = mid
+		else:
+			left = mid+1 # indexを考慮して+1
+	print(left)
